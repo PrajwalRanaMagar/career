0e475df5d95de7b1c94ef91e9f7021f5645dc012
Susank Baniya <susankbaniya12345@gmail.com>
2025-06-22 22:17:44 +0545

    final view of apply and homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,30 +1,24 @@
 import React from "react";
-import Styles from "./Home.module.css";
+import styles from "./Home.module.css";
 import Cards from "../../components/cards/cards";
-import JobCards, { JobCard } from "../../components/cards/JobCards";
 
 const Home = () => {
   return (
-    <div>
-      <div className={Styles["home-page"]}>
-        <div className={Styles["home-wrapper"]}>
-          <div className={Styles["home-first"]}>
-            <div className={Styles["home-first-div"]}>
-              <div className={Styles["home-first-container"]}>
-                We're hiring!
-              </div>
-              <h1 className={Styles["home-first-heading"]}>
-                Be part of our mission
-              </h1>
-              <p className={Styles["home-first-paragraph"]}>
+    <div className={styles.homepage}>
+      <div className={styles.page}>
+        <div>
+          <div className={styles.wrapper}>
+            <div className={styles.first}>
+              <div className={styles.firstContainer}>We're hiring!</div>
+              <h1 className={styles.firstHeading}>Be part of our mission </h1>
+              <p className={styles.firstParagraph}>
                 We're looking for passionate people to join us on our mission.
                 We value flat hierarchies, clear communication, and full
                 ownership and responsibility.
               </p>
             </div>
-            <div className={Styles["home-image"]}></div>
+            <div className={styles.homeImage}></div>
           </div>
-
           <Cards />
         </div>
       </div>
